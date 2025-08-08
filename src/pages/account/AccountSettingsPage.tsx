b1a25c21e6ae1336018fce83e68a9b44e47c4b0b
Jordon Harrison <Jordon.Harrison@outlook.com>
2025-08-08 17:07:45 +0100

    refactor: Simplify layout structure of AccountSettingsPage by removing unnecessary wrapper div

--- src/pages/account/AccountSettingsPage.tsx
+++ src/pages/account/AccountSettingsPage.tsx
@@ -239,9 +239,8 @@ const AccountSettingsPage: React.FC = () => {
   };
   
   return (
-    <div className="min-h-screen bg-background p-4">
-      <div className="w-full max-w-5xl mx-auto">
-        <h1 className="text-2xl font-bold mb-6 text-[rgb(var(--color-text))]">Account Settings</h1>
+    <div className="max-w-5xl mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-6 text-[rgb(var(--color-text))]">Account Settings</h1>
       
       <div className="flex flex-col md:flex-row gap-6">
         {/* Sidebar with tabs */}
@@ -286,6 +285,7 @@ const AccountSettingsPage: React.FC = () => {
               Theme & Appearance
             </Button>
           </nav>
+        </div>
         
         {/* Main content area */}
         <div className="flex-1">
@@ -813,8 +813,6 @@ const AccountSettingsPage: React.FC = () => {
               </CardContent>
             </Card>
           )}
-        </div>
-      </div>
         </div>
       </div>
     </div>
